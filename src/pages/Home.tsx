e8e52592b92879b1ac434330fcfbc4ab7f8bdfc0
tony007x <aekanan2004@gmail.com>
2025-02-19 20:46:29 +0700

    done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,25 +3,22 @@ import PopularShirt from "@/components/popularShirt";
 function Home() {
   return (
     <>
-      <div className="flex w-full h-screen justify-center items-center overflow-hidden ">
+      <div className="flex w-full h-screen justify-center items-center relative overflow-hidden">
         {/* Background Image */}
         {/* ใช้ absolute inset-0 → ทำให้ div พื้นหลัง ขยายเต็มพื้นที่ ของ parent */}
         <div
-          className="absolute inset-0 bg-cover bg-center bg-no-repeat bg-fixed max-lg:bg-scroll"
+          className="absolute inset-0 bg-cover bg-center bg-no-repeat bg-fixed"
           style={{ backgroundImage: `url(${'images/bg-landing.jpg'})`,
           filter: 'brightness(0.3)',
+          backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
           backgroundAttachment: 'fixed',
-          maxWidth: '100vw',
-          maxHeight: '100vh',
          }}
         />
 
         {/* Content */}
         <h1 className="text-white relative z-10">sec1</h1>
-        {/* ทำยังไงดี */}
-        {/* <img src="images/bg-landing.jpg" alt="bg-landing" className="w-full h-screen bg-cover bg-no-repeat bg-fixed" /> */}
       </div>
 
       {/* Section 2 */}
