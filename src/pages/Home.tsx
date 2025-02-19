1312b61ceacacc64a5a43cb8cbd601ec7f672d38
tony007x <aekanan2004@gmail.com>
2025-02-19 20:38:18 +0700

    try to fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,11 @@ import PopularShirt from "@/components/popularShirt";
 function Home() {
   return (
     <>
-      <div className="flex w-full h-screen justify-center items-center relative overflow-hidden">
+      <div className="flex w-full h-screen justify-center items-center overflow-hidden ">
         {/* Background Image */}
         {/* ใช้ absolute inset-0 → ทำให้ div พื้นหลัง ขยายเต็มพื้นที่ ของ parent */}
         <div
-          className="absolute inset-0 bg-cover bg-center bg-no-repeat bg-fixed"
+          className="absolute inset-0 bg-cover bg-center bg-no-repeat bg-fixed max-lg:bg-scroll"
           style={{ backgroundImage: `url(${'images/bg-landing.jpg'})`,
           filter: 'brightness(0.3)',
           backgroundPosition: 'center',
@@ -20,6 +20,8 @@ function Home() {
 
         {/* Content */}
         <h1 className="text-white relative z-10">sec1</h1>
+        {/* ทำยังไงดี */}
+        {/* <img src="images/bg-landing.jpg" alt="bg-landing" className="w-full h-screen bg-cover bg-no-repeat bg-fixed" /> */}
       </div>
 
       {/* Section 2 */}
